1e7679abbb0f7d3a2cfc9888ad039f360c8ba05a
FrensysThomas87 <delta873@gmail.com>
2021-01-27 16:43:47 +0100

    Stampati i dots a video tramite v-for

--- main.js
+++ main.js
@@ -4,16 +4,23 @@
 var app = new Vue({
  el: '#root',
  data:{
+   // Array di immagini
    imgs:[
      'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
      'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
    ],
-   photosIdx:0
+
+
+   circle: ['fas fa-circle', 'fas fa-circle','fas fa-circle','fas fa-circle'],
+   // Index per le foto
+   photosIdx:0,
+   dotIndex:0
  },
 
  methods:{
+   // Metodo che va avanti quando clicco su next, ma torna alla prima foto dopo l'ultima
    nextPhoto:function(){
      console.log(this.photosIdx);
 
@@ -23,10 +30,13 @@ var app = new Vue({
        this.photosIdx += 1;
      }
    },
+
+   // Metodo che va indietro quando clicco su prev, ma va all'ultima se sono sulla prima
    prevPhoto:function(){
      console.log(this.photosIdx);
      if(this.photosIdx === 0){
        this.photosIdx = this.imgs.length - 1;
+
      }else{
        this.photosIdx -= 1;
      }
